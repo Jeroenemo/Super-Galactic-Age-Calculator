9ad783da1d73d03ca886b8a5ea6bc1bf9b6f5fa3
Jeroen van Seeters <vanseetersjeroen@gmail.com>
2021-02-05 12:56:58 -0800

    pass refactor yearsLeft: mars test

--- __tests__/calculator.tests.js
+++ __tests__/calculator.tests.js
@@ -44,7 +44,7 @@ describe('Calculator', () => {
   });
 
   test('should return years left if planet is mars', () => {
-    expect(calculator.yearsLeft("mars")).toEqual(34);
+    expect(calculator.yearsLeft("mars")).toEqual("You have 34 years to live");
   });
   test('should return years left if planet is jupiter', () => {
     expect(calculator.yearsLeft("jupiter")).toEqual("You have 5 years to live");
